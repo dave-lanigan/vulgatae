38a21512fff8605d4e91d2d7fe49f472f9ffffe1
David Lanigan <david.lanigan@bayer.com>
2025-08-11 23:53:34 -0400

    fix: debugging

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,10 @@
+import path from 'path'
+
 export default defineEventHandler(() => {
   return {
     aboutAPI: "API for the Vulgate/Douay-Rheims Bible",
     about: "The Vulgate is a version of the Bible",
-    contact: "bohemdev@tutanota.com"
+    contact: "bohemdev@tutanota.com",
+    dbSourcePath: path.resolve(process.cwd(), 'server/api/v.db')
   }
 })
