982891db51f56d5488f9837f3b87058967deffda
David Lanigan <david.lanigan@bayer.com>
2025-09-02 23:13:16 -0400

    feat: Add pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,18 +5,63 @@ export default defineNuxtConfig({
     head: {
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [
+        { rel: 'manifest', href: '/manifest.webmanifest' },
+        { rel: 'apple-touch-icon', href: '/icons/icons/web/apple-touch-icon.png' }
       ]
     }
   },
   modules: [
     '@nuxt/icon',
     '@nuxtjs/color-mode',
-    '@nuxt/content'
+    '@nuxt/content',
+    '@vite-pwa/nuxt',
   ],
   vite: {
     plugins: [
       tailwindcss()
     ],
+  },
+  pwa: {
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true,
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+      globIgnores: [
+        '**/node_modules/**/*',
+        'sw.js',
+        'workbox-*.js',
+        '**/_payload.json',
+        '_nuxt/builds/**/*.json'
+      ]
+    },
+    manifest: {
+      name: 'Vulgatae.com',
+      short_name: 'Vulgatae',
+      description: 'A Latin focused Bible app for studying the Vulgate.',
+      theme_color: '#b6862c',
+      background_color: '#ffffff',
+      display: 'standalone',
+      orientation: 'portrait',
+      scope: '/',
+      start_url: '/',
+      lang: 'en',
+      icons: [
+        {
+          src: '/icons/icons/web/icon-192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/icons/web/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        }
+      ]
+    }
   },
   css: ["~/assets/app.css"],
   compatibilityDate: '2025-08-10',
---
+++ roadmap.md
@@ -0,0 +1,28 @@
+## Roadmap
+### Features
+- [] Daily bible verse container
+- [] Pray the psalms section
+  - [] [psalms-english](https://www.youtube.com/watch?v=L4_ehC_VEPc&list=PLTznj9fyCSlDWxCmrBpOfEkXwIeWIe3I0&index=2&ab_channel=ChantsandRants)
+- [] Daily Mass Readings (Traditional)
+  - [] ETL DMR
+- [] Commentaries
+  - [] St. Aquinas
+  - [] St. Augustine
+  - [] St. Jerome
+  - [] Origen
+- [] Add a prayers sections
+- [] Add text to speech for the entire bible
+- [] Add chatGPT Search
+- [] Each word translated
+- [] AI Bible Verse Search + trending searches
+
+### Fixes
+- [] Cannot type in a book/chapter/verse to go to it
+- [] Fix CSS for finding verses
+- [] Fix chapters so the words arent hidden
+- [] light and dark theme for site - add toggle for this.
+- [] Other
+  - [] add tooltips
+  - [] use local storage + add my vulgatae that tracks verses you read
+  - [] navigation to the next chapter at the bottom of the 
+  - [] fix theme overall. come up with something nice and fancy.
